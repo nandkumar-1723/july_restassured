181dc32a79a5cc984c175df0be2f6bd04309df66
nandkumar-1723 <127601507+nandkumar-1723@users.noreply.github.com>
2024-11-28 18:14:44 +0530

    28th Nov 2024

--- src/test/java/TestCases/UserStory.java
+++ src/test/java/TestCases/UserStory.java
@@ -1,7 +1,6 @@
 package TestCases;
 
 import io.restassured.*;
-import io.restassured.http.*;
 import io.restassured.response.*;
 import org.json.*;
 import org.json.simple.parser.*;
@@ -20,12 +19,12 @@ public class UserStory {
 
     @Test(priority = 1)
     public void loginJira() throws IOException, ParseException {
-        FileReader fr = new FileReader("/home/nandkumar/Videos/July_RestAssured/src/main/java/com/arise/Files/jiraLogin.json");
+        FileReader fr = new FileReader("/home/nandkumar/Videos/july_restassured/src/main/java/com/arise/Files/jiraLogin.json");
         JSONParser jp = new JSONParser();
         String requestBody = jp.parse(fr).toString();
         System.out.println(requestBody);
 
-            Response response = RestAssured.given().baseUri("http://localhost:9009/").body(requestBody)
+            Response response = RestAssured.given().baseUri("http://localhost:9008/").body(requestBody)
                     .header("Content-Type","application/json")
                 .when().post("/rest/auth/1/session").then().extract().response();
 
@@ -40,11 +39,11 @@ public class UserStory {
     public void createUserStory() throws IOException, ParseException {
         System.out.println(cookie);
 
-        FileReader fr = new FileReader("/home/nandkumar/Videos/July_RestAssured/src/main/java/com/arise/Files/createUserStory.json");
+        FileReader fr = new FileReader("/home/nandkumar/Videos/july_restassured/src/main/java/com/arise/Files/createUserStory.json");
         JSONParser jp = new JSONParser();
         String requestBody = jp.parse(fr).toString();
 
-        Response response = RestAssured.given().baseUri("http://localhost:9009/").body(requestBody)
+        Response response = RestAssured.given().baseUri("http://localhost:9008/").body(requestBody)
                 .header("Content-Type", "application/json")
                 .header("Cookie",cookie).when().post("/rest/api/2/issue")
                 .then().extract().response();
@@ -60,7 +59,7 @@ public class UserStory {
     @Test(priority = 3)
     public void getUserStory(){
 
-        Response response =RestAssured.given().baseUri("http://localhost:9009/")
+        Response response =RestAssured.given().baseUri("http://localhost:9008/")
                 .header("Content-Type", "application/json")
                 .header("Cookie",cookie).when().get("/rest/api/2/issue/"+issueID)
                 .then().extract().response();
@@ -68,6 +67,32 @@ public class UserStory {
         System.out.println(response.getStatusCode());
         System.out.println(response.asString());
 
+    }
+
+    @Test(priority = 4)
+    public void updateUserStory() throws IOException, ParseException {
+        FileReader fr = new FileReader("/home/nandkumar/Videos/july_restassured/src/main/java/com/arise/Files/createUserStory.json");
+        JSONParser jp = new JSONParser();
+        String requestBody = jp.parse(fr).toString();
+
+        JSONObject updatedJson = new JSONObject(requestBody);
+        updatedJson.getJSONObject("fields").put("summary","updating the user story");
+
+        Response response = RestAssured.given().baseUri("http://localhost:9008/").header("Content-Type", "application/json")
+                .header("Cookie", cookie).body(updatedJson.toString()).when().put("/rest/api/2/issue/"+issueID)
+                .then().extract().response();
+
+        System.out.println(response.getStatusCode());
+    }
+
+    @Test(priority = 5)
+    public void deleteUserStory(){
+
+        Response response = RestAssured.given().baseUri("http://localhost:9008/").header("Content-Type", "application/json")
+                .header("Cookie", cookie).when().delete("/rest/api/2/issue/" + issueID)
+                .then().extract().response();
+
+        System.out.println(response.getStatusCode());
 
     }
     
